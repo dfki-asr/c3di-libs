f8d7ed7b068dc45ccfff2f162d572f5dfa77c031
Christian Schlinkmann <chsc16@dfki.de>
2021-09-03 12:01:03 +0200

    Change FindFreeImage module to correctly identify existing libraries on Linux

--- cmake/FindFreeImage.cmake
+++ cmake/FindFreeImage.cmake
@@ -16,16 +16,36 @@ IF (WIN32)
         ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/FreeImage/lib/x64
         DOC "The FreeImagePlus library")
 ELSE (WIN32)
-   set(FREEIMAGE_ROOT_DIR "${C3DI_LIBS_DEPENDENCIES_ROOT}/FreeImage/Linux/")
-   set(FREEIMAGEPLUS_INCLUDE_PATH ${FREEIMAGE_ROOT_DIR})
-   set(FREEIMAGEPLUS_LIBRARY ${FREEIMAGE_ROOT_DIR}/x64/libFreeImagePlus.so)
+   # Find include files
+    find_path(
+        FREEIMAGEPLUS_INCLUDE_PATH
+        NAMES FreeImagePlus.h
+        PATHS
+            /usr/include
+            /usr/local/include
+            /sw/include
+            /opt/local/include
+        DOC "The directory where FreeImage.h resides")
+
+    # Find library files
+    find_library(
+        FREEIMAGEPLUS_LIBRARY
+        NAMES freeimageplus
+        PATHS
+            /usr/lib64
+            /usr/lib
+            /usr/local/lib64
+            /usr/local/lib
+            /usr/lib/x86_64-linux-gnu
+            /opt/local/lib
+            ${FREEIMAGE_ROOT_DIR}/lib
+        DOC "The FreeImage library")
 ENDIF (WIN32)
 
 SET(FREEIMAGEPLUS_LIBRARIES ${FREEIMAGEPLUS_LIBRARY})
 IF (FREEIMAGEPLUS_INCLUDE_PATH AND FREEIMAGEPLUS_LIBRARY) 
         SET( FREEIMAGEPLUS_FOUND TRUE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise")
         message("Found FreeImagePlus")
-        message("${FREEIMAGEPLUS_INCLUDE_PATH}")
 ELSE (FREEIMAGEPLUS_INCLUDE_PATH AND FREEIMAGEPLUS_LIBRARY)
         SET( FREEIMAGEPLUS_FOUND FALSE CACHE BOOL "Set to TRUE if FreeImage is found, FALSE otherwise")
         message("FreeImagePlus NOT FOUND")    
